7b90817183205fae722c6bfd1053830f03403638
amineBd <aminebenmade1999@gmail.com>
2024-09-02 10:37:41 +0100

    add setters/getters to Reservation Class

--- src/hotelmanagementsystem/Reservation.java
+++ src/hotelmanagementsystem/Reservation.java
@@ -3,16 +3,64 @@ package hotelmanagementsystem;
 import java.time.LocalDate;
 
 public class Reservation {
-    private String Client;
+
+    //fields
+    private String client;
     private Chambre chambre;
     private LocalDate checkIn;
     private LocalDate checkOut;
 
-    public Reservation(String Client, Chambre chambre, LocalDate checkIn, LocalDate checkOut){
-        this.Client = Client;
+
+    //constructeur
+    public Reservation(String client, Chambre chambre, LocalDate checkIn, LocalDate checkOut){
+        this.client = client;
         this.chambre = chambre;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
         this.chambre.setDisponible(false);
+    }
+
+    //Getters
+    public String getClient() {
+        return client;
+    }
+
+    public Chambre getChambre() {
+        return chambre;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    //Setters
+    public void setClient(String client){
+        this.client = client;
+    }
+
+    public void setCheckIn(LocalDate checkIn){
+        this.checkIn = checkIn;
+    }
+
+    public void setCheckOut(LocalDate checkOut){
+        this.checkOut = checkOut;
+    }
+
+    public void setChambre(Chambre Modifchambre){
+        if(Modifchambre.isDisponible()){
+            this.chambre.setDisponible(true);
+            this.chambre = Modifchambre;
+            this.chambre.setDisponible(false);
+        } else {
+            System.out.println("Cette chambre deja reserver");
+        }
+    }
+
+    public void annuler(){
+        this.chambre.setDisponible(true);
     }
 }
