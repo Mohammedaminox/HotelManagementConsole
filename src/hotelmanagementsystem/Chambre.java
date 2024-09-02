38a4b38f6358ea46bdef53770f802d7d1cee49f2
amineBd <aminebenmade1999@gmail.com>
2024-09-02 07:41:23 +0100

    add Chambre Class

---
+++ src/hotelmanagementsystem/Chambre.java
@@ -0,0 +1,27 @@
+package hotelmanagementsystem;
+
+public class Chambre {
+    private int chambreID;
+    private boolean disponible;
+
+    public Chambre() {}
+    public Chambre(int chambreID) {
+        this.chambreID = chambreID;
+        this.disponible = true;
+    }
+
+    public int getChambreID() {
+        return chambreID;
+    }
+
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+
+}
